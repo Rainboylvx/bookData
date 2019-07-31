eed9131f2ce64b6a017e9a49dc4e5c0ad1e3c8e1
rainboy <rainboylvx@qq.com>
2019-07-31 13:25:10 +0000

    update

--- 算法篇/分治/二分查找/first_g_template.cpp
+++ 算法篇/分治/二分查找/first_g_template.cpp
@@ -1,11 +1,11 @@
-//查找范围是[l,r), a[r] 永远 >= key
+//查找范围是[l,r), a[r] 永远 > key
 template <typename T>
-int first_g(T key,T a[],int l,int r){
+int first_ge(T key,T a[],int l,int r){
     int m;
     while( l != r  ) //表示l和r没有重合
     {
         m = (l+r) >>1; // 取中间位置
-        if( a[m] < key ) //表示 [m+1,r) 满足条件
+        if( a[m] <= key ) //表示 [m+1,r) 满足条件
             l = m+1;
         else
             r = m;
